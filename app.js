a03d17f414c066e0d1d7a5f77ef6a7d55a5ffb93
Aleksa Ojdanic <aojdanic97@gmail.com>
2025-06-29 02:14:06 +0200

    the selects now work on change, without the need to submit the form

--- app.js
+++ app.js
@@ -3,19 +3,24 @@ import express from "express";
 const app = express();
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/:page", async (req, res) => {
   const pageNumber = Number.parseInt(req.params?.page);
   const page = Number.isNaN(pageNumber) ? 1 : pageNumber;
-  const name = req.query?.name;
 
-  console.log("request query params", req.query);
+  const name = req.query?.name ? `&name=${req.query.name}` : "";
+  const species = req.query?.species ? `&species=${req.query.species}` : "";
+  const status = req.query?.status ? `&status=${req.query.status}` : "";
+  const gender = req.query?.gender ? `&gender=${req.query.gender}` : "";
 
   const response = await fetch(
-    `https://rickandmortyapi.com/api/character?page=${page}${name ? `&name=${name}` : ""}`,
+    `https://rickandmortyapi.com/api/character?page=${page}${name}${species}${status}${gender}`,
   );
 
-  const { results, info } = await response.json();
+  const data = await response.json();
+
+  const results = data?.results ?? [];
 
   res.send(`
     <!DOCTYPE html>
@@ -53,71 +58,85 @@ app.get("/:page", async (req, res) => {
 
         <header>
           <div class="container">
-            <div class="logo-image-container">
-              <img src="/rick_and_morty_logo.png" alt="rick-and-morty-logo image"/>
-            </div>
+            <a href="/1">
+              <div class="logo-image-container">
+                <img src="/rick_and_morty_logo.png" alt="rick-and-morty-logo image"/>
+              </div>
+            </a>
 
             <div class="search-input-wrapper">
+              <div class="search-input-icon">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                  <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/>
+                </svg>
+              </div>
+
               <input
                 type="text"
                 name="name"
                 class="search-input"
-                value="${name ?? ""}"
+                value="${req.query?.name ?? ""}"
                 placeholder="Search for a character"
-                hx-get="/${pageNumber}"
+                hx-post="/filters"
                 hx-trigger="keyup changed delay:500ms"
-                hx-include="this"
+                hx-target="div.grid"
+                hx-select="div.grid"
+                hx-swap="outerHTML"
                 hx-push-url="true"
               />
-              <div class="dropdown"></div>
             </div>
           </div>
         </header>
 
         <main>
 
-    <form>
+    <form 
+      method="POST"
+      action="/filters"
+      hx-target="div.grid"
+      hx-select="div.grid"
+      hx-swap="outerHTML"
+    >
       <label>
         <span>Species</span>
 
-        <select name="species" id="species">
-          <option value="">Select a species</option>
-          <option value="human">Human</option>
-          <option value="alien">Alien</option>
-          <option value="humanoid">Humanoid</option>
-          <option value="unknown">unknown</option>
-          <option value="poopybutthole">Poopybutthole</option>
-          <option value="mythological">Mythological Creature</option>
-          <option value="animal">Animal</option>
-          <option value="robot">Robot</option>
-          <option value="cronenberg">Cronenberg</option>
-          <option value="disease">Disease</option>
+        <select  name="species" id="species" hx-post="/filters">
+          <option ${!req.query?.species ? "selected" : ""} value="">Select a species</option>
+          <option ${req.query?.species === "human" ? "selected" : ""} value="human">Human</option>
+          <option ${req.query?.species === "alien" ? "selected" : ""} value="alien">Alien</option>
+          <option ${req.query?.species === "humanoid" ? "selected" : ""} value="humanoid">Humanoid</option>
+          <option ${req.query?.species === "unknown" ? "selected" : ""} value="unknown">unknown</option>
+          <option ${req.query?.species === "poopybutthole" ? "selected" : ""} value="poopybutthole">Poopybutthole</option>
+          <option ${req.query?.species === "mythological" ? "selected" : ""} value="mythological">Mythological Creature</option>
+          <option ${req.query?.species === "animal" ? "selected" : ""} value="animal">Animal</option>
+          <option ${req.query?.species === "robot" ? "selected" : ""} value="robot">Robot</option>
+          <option ${req.query?.species === "cronenberg" ? "selected" : ""} value="cronenberg">Cronenberg</option>
+          <option ${req.query?.species === "disease" ? "selected" : ""} value="disease">Disease</option>
         </select>
       </label>
 
       <label>
         <span>Status</span>
 
-        <select name="status" id="status">
-          <option value="">Select a status</option>
-          <option value="alive">alive</option>
-          <option value="dead">dead</option>
-          <option value="unknown">unknown</option>
+        <select hx-post="/filters" name="status" id="status">
+          <option ${!req.query?.status ? "selected" : ""} value="">Select a status</option>
+          <option ${req.query?.status === "alive" ? "selected" : ""} value="alive">alive</option>
+          <option ${req.query?.status === "dead" ? "selected" : ""} value="dead">dead</option>
+          <option ${req.query?.status === "unknown" ? "selected" : ""} value="unknown">unknown</option>
         </select>
       </label>
 
       <label>
         <span>Gender</span>
-        <select name="gender" id="gender">
-          <option value="">Select a gender</option>
-          <option value="female">female</option>
-          <option value="male">male</option>
-          <option value="genderless">genderless</option>
-          <option value="unknown">unknown</option>
+        <select name="gender" id="gender" hx-post="/filters">
+          <option ${!req.query.gender ? "selected" : ""} value="">Select a gender</option>
+          <option ${req.query?.gender === "female" ? "selected" : ""} value="female">female</option>
+          <option ${req.query?.gender === "male" ? "selected" : ""} value="male">male</option>
+          <option ${req.query?.gender === "genderless" ? "selected" : ""} value="genderless">genderless</option>
+          <option ${req.query?.gender === "unknown" ? "selected" : ""} value="unknown">unknown</option>
         </select>
       </label>
 
-      <button>Apply filters</button>
     </form>
           <div class="container">
             <div class="grid">
@@ -126,9 +145,6 @@ app.get("/:page", async (req, res) => {
                   (result) =>
                     `<a href="/character/${result.id}">
                       <div class="card">
-                        <!-- <p class="${result.status === "unknown" ? "status_pad" : "status"}">
-                           ${result.status === "unknown" ? "Unknown" : result.status}
-                         </p> -->
                         <div>
                           <img
                             class="card-img"
@@ -219,10 +235,33 @@ app.get("/character/:id", async (req, res) => {
 
         <header>
           <div class="container">
-            <div class="logo-image-container">
-              <img src="/rick_and_morty_logo.png" alt="rick-and-morty-logo image"/>
+            <a href="/1">
+              <div class="logo-image-container">
+                <img src="/rick_and_morty_logo.png" alt="rick-and-morty-logo image"/>
+              </div>
+            </a>
+
+            <div class="search-input-wrapper">
+              <div class="search-input-icon">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                  <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/>
+                </svg>
+              </div>
+
+              <input
+                type="text"
+                name="name"
+                class="search-input"
+                value="${req.query?.name ?? ""}"
+                placeholder="Search for a character"
+                hx-post="/filters"
+                hx-trigger="keyup changed delay:500ms"
+                hx-target="div.grid"
+                hx-select="div.grid"
+                hx-swap="outerHTML"
+                hx-push-url="true"
+              />
             </div>
-            <input type="search"/>
           </div>
         </header>
 
@@ -258,6 +297,19 @@ app.get("/character/:id", async (req, res) => {
         </footer>
       </body>
     </html>`);
+});
+
+app.post("/filters", async (req, res) => {
+  console.log("req.query", req.query);
+  console.log("req.body", req.body);
+  const searchParamsString = Object.keys(req.body)
+    .filter((key) => Boolean(req.body[key]))
+    .map((key) => `${key}=${req.body[key]}`)
+    .join("&");
+
+  const searchString = searchParamsString ? `?${searchParamsString}` : "";
+
+  res.redirect(`/1${searchString}`);
 });
 
 app.listen(3000);
